8b6d967cd7e32253d7d69400d1577439e8925453
Ageng Dwi Prastyawan <agengdp@icloud.com>
2017-06-03 01:31:49 +0700

    penambahan sejenis remote image

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
         browner12\helpers\HelperServiceProvider::class, 
         Conner\Tagging\Providers\TaggingServiceProvider::class,
         Laravelista\Ekko\EkkoServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class
 
     ],
 
@@ -231,7 +232,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Image' => Intervention\Image\Facades\Image::class
     ],
 
 ];
