3e683be8d9e909232da065978b054c7e65fdb243
Ageng Dwi Prastyawan <agengdp@icloud.com>
2017-06-15 16:42:59 +0700

    garap movie

--- app/Http/Controllers/FrontMovieController.php
+++ app/Http/Controllers/FrontMovieController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class FrontMovieController extends Controller
 {
-    //
+    public function index($slug){
+
+    	$movie = Movie::where('slug', $slug)->first();
+        
+        $videos = collect(json_decode($movie->links));
+    	
+    	foreach($videos as $stream){
+          $stream->video_stream_id = substr($stream->video_url, strrpos($stream->video_url, '/') + 1); // Assign video id
+          $stream->video_quality = rtrim($stream->video_quality, 'p');
+        }
+        
+        $mp4_links = $videos->where('video_type', 'mp4');
+        $mkv_links = $videos->where('video_type', 'mkv');
+
+    	return view('public.playMovie', [
+    		'movie' => $movie,
+	        'mp4_links' => $mp4_links->sortBy('video_quality'),
+	        'mkv_links' => $mkv_links->sortBy('video_quality')
+    	]);
+    }
 }
