5405a09b86bcf9b6cafec7d5a0f863734c930a20
Ageng Dwi Prastyawan <agengdp@icloud.com>
2017-07-04 18:48:26 +0700

    viewport

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>@yield('title')</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="{{asset('storage/assets/bootstrap/css/bootstrap.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('storage/assets/css/main.css')}}">
 </head>
