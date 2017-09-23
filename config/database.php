e26d1bd1db09d6a9f2e31e3925cdd8cfd0d667b1
Ageng Dwi Prastyawan <agengdp@icloud.com>
2017-09-24 02:44:25 +0700

    implement testing unit

--- config/database.php
+++ config/database.php
@@ -39,6 +39,18 @@ return [
             'prefix' => '',
         ],
 
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'database'  => env('DB_TEST_DATABASE', 'maulsama_test'),
+            'username'  => env('DB_TEST_USERNAME', 'homestead'),
+            'password'  => env('DB_TEST_PASSWORD', 'secret'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
