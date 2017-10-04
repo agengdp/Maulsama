0b4d21e3d54a5def8545a3a2a6d4fca80634bce2
Ageng Dwi Prastyawan <agengdp@icloud.com>
2017-10-05 03:20:27 +0700

    fix crud episode

--- app/Http/Controllers/Admin/EpisodeController.php
+++ app/Http/Controllers/Admin/EpisodeController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Media;
 use App\Episode;
-use App\Series;
+use App\DownloadLink;
 
 class EpisodeController extends Controller
 {
@@ -19,8 +20,7 @@ class EpisodeController extends Controller
     public function store(Request $request, $id)
     {
         $the_episode = new Episode;
-
-        $series = Series::find($id);
+        $series = Media::find($id);
         $the_episode->series()->associate($series); //ini sama halnya memasukkan series_id dengan series yang ditambahkan
 
         if ($request->hasFile('cover-episode')) {
@@ -32,12 +32,22 @@ class EpisodeController extends Controller
         $the_episode->episode = $request->episode;
         $the_episode->judul_episode = $request->judul_episode;
         $the_episode->spoiler = $request->spoiler;
-        $the_episode->links = json_encode($request->episode_list);
 
         $the_episode->save();
 
-        flash('Episode berhasil di tambahkan')->success();
+        // build download link yang akan di attach dengan episode
+        // di loop karena menggunakan save Many, biar nggak simpen satu2
+        foreach ($request->episode_list as $value) {
+            $download_link[] = new DownloadLink([
+            'type'            => 'episode',
+            'video_type'      => $value['video_type'],
+            'video_quality'   => $value['video_quality'],
+            'video_url'       => $value['video_url'],
+          ]);
+        }
 
+        $the_episode->download_links()->saveMany($download_link);
+        flash('Episode berhasil di tambahkan')->success();
         return redirect()->route('series.show', ['id' => $id]);
     }
 
@@ -52,8 +62,7 @@ class EpisodeController extends Controller
     {
         $the_episode = Episode::find($episode_id);
 
-        $series = Series::find($series_id);
-
+        $series = Media::find($series_id);
         $the_episode->series()->associate($series);
 
         if ($request->hasFile('edit-episode-cover')) {
@@ -63,15 +72,26 @@ class EpisodeController extends Controller
             $the_episode->cover = $image_file_name[1];
         }
 
-        $the_episode->episode = $request->episode;
+        $the_episode->episode       = $request->episode;
         $the_episode->judul_episode = $request->judul_episode;
-        $the_episode->spoiler = $request->spoiler;
-        $the_episode->links = json_encode($request->episode_list);
+        $the_episode->spoiler       = $request->spoiler;
 
         $the_episode->save();
+        $the_episode->download_links()->delete();
 
+        // build download link yang akan di attach dengan episode
+        // di loop karena menggunakan save Many, biar nggak simpen satu2
+        foreach ($request->episode_list as $value) {
+            $download_link[] = new DownloadLink([
+            'type'            => 'episode',
+            'video_type'      => $value['video_type'],
+            'video_quality'   => $value['video_quality'],
+            'video_url'       => $value['video_url'],
+          ]);
+        }
+
+        $the_episode->download_links()->saveMany($download_link);
         flash('Episode <strong>'.$the_episode->episode.'</strong> berhasil di update')->success();
-
         return redirect()->route('series.show', ['id' => $series_id]);
     }
 
@@ -84,11 +104,8 @@ class EpisodeController extends Controller
      */
     public function destroy($series_id, $episode_id)
     {
-        $the_episode = Episode::find($episode_id);
-        $episode_ke = $the_episode->episode;
-
-        Storage::delete('public/'.$the_episode->cover); // hapus covernya
-
+        $the_episode  = Episode::find($episode_id);
+        $episode_ke   = $the_episode->episode;
         $the_episode->delete();
 
         flash('Episode ke <strong>'.$episode_ke.'</strong> telah berhasil dihapus')->success();
