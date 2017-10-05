19c8a1ffa08d9e2b3b84c6e2ab5329b84c4cce3e
Ageng Dwi Prastyawan <agengdp@icloud.com>
2017-10-05 23:27:46 +0700

    Styling footer

--- resources/views/public/includes/footer.blade.php
+++ resources/views/public/includes/footer.blade.php
@@ -1,7 +1,61 @@
 <div class="container">
 	<div class="row">
-		<div class="col-md-12">
-			Copyright © Animestream. All Rights Reserved
+		<div class="col-md-4">
+			<img src="https://www.ratakan.com/wp-content/uploads/2014/04/ratakan-logo-footer.png">
+			<p>Made with <i class="glyphicon glyphicon-heart text-danger"></i> from Surabaya, Indonesia</p>
+			<p>Copyright © Maulsama. All Rights Reserved</p>
 		</div>
+		<div class="col-md-2">
+				<div class="footer-widget">
+					<div class="footer-widget__title">
+						Info
+					</div>
+					<!-- /.footer-widget__title -->
+					<div class="footer-widget__content">
+						<ul>
+							<li>About Us</li>
+							<li>DMCA</li>
+						</ul>
+					</div>
+					<!-- /.footer-widget__content -->
+				</div>
+				<!-- /.footer-widget -->
+		</div>
+		<div class="col-md-2">
+			<div class="footer-widget">
+				<div class="footer-widget__title">
+					Panduan
+				</div>
+				<!-- /.footer-widget__title -->
+				<div class="footer-widget__content">
+					<ul>
+						<li>Cara Download</li>
+						<li>Laport Link Rusak / Error</li>
+					</ul>
+				</div>
+				<!-- /.footer-widget__content -->
+			</div>
+			<!-- /.footer-widget -->
+		</div>
+		<!-- /.col-md-3 -->
+		<div class="col-md-4">
+			<div class="footer-widget">
+				<div class="footer-widget__title">
+					Connect with us
+				</div>
+				<!-- /.footer-widget__title -->
+				<div class="footer-widget__content footer-widget__content--connect">
+					<ul>
+						<li class="fb">Facebook</li>
+						<li class="twit">Twitter</li>
+						<li class="ig">Instagram</li>
+						<li class="gplus">Google+</li>
+					</ul>
+				</div>
+				<!-- /.footer-widget__content -->
+			</div>
+			<!-- /.footer-widget -->
+		</div>
+		<!-- /.col-md-4 -->
 	</div>
 </div>
