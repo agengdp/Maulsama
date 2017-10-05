21d281cf8431a7a7b117908dc65b2f97fd054f0a
Ageng Dwi Prastyawan <agengdp@icloud.com>
2017-10-05 16:22:43 +0700

    benerin ini

--- resources/views/public/includes/relatedEpisode.blade.php
+++ resources/views/public/includes/relatedEpisode.blade.php
@@ -4,16 +4,19 @@
     <div class="row">
       <div class="col-md-12">
         <h3 class="header header--related">Anime Menarik Lainnya !</h3>
-        @foreach ($series->genre->random()->series as $seri)
-          <div class="col-xs-6 col-md-2 col-lg-2 related__container">
-            <a href="{{ route('frontSeries', $seri->slug) }}" class="thumbnail related__link">
-              <img src="{{ asset("images/vert/$seri->cover" ) }}" alt="{{ $seri->title }}" class="related__image">
-                <div class="caption related__caption">
-                  <span class="caption-text related__judul">{{ $seri->title }}</span>
-                </div> {{-- /.caption --}}
-            </a>
-          </div>  {{-- .col-xs-6 .col-md-4 .col-lg-3 .image-container --}}
-        @endforeach
+
+        {{-- @if(count($series->genre->random()->media) > 0) --}}
+          @foreach ($series->genre->random()->media as $seri)
+            <div class="col-xs-6 col-md-2 col-lg-2 related__container">
+              <a href="{{ route('frontSeries', $seri->slug) }}" class="thumbnail related__link">
+                <img src="{{ asset("images/vert/$seri->cover" ) }}" alt="{{ $seri->title }}" class="related__image">
+                  <div class="caption related__caption">
+                    <span class="caption-text related__judul">{{ $seri->title }}</span>
+                  </div> {{-- /.caption --}}
+              </a>
+            </div>  {{-- .col-xs-6 .col-md-4 .col-lg-3 .image-container --}}
+          @endforeach
+        {{-- @endif --}}
       </div>
       <!-- /.col-md-12 -->
     </div>
