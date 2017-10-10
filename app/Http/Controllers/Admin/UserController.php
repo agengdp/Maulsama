189fff1cbb87eaa0d0f9bad74d229574eef24c4e
Ageng Dwi Prastyawan <agengdp@icloud.com>
2017-10-10 09:04:11 +0700

    added user crud

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
+use App\Role;
+use Auth;
 
 class UserController extends Controller
 {
@@ -14,7 +17,11 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return view('admin/user/user', [
+          'heading' => 'Users',
+          'users' => $users,
+        ]);
     }
 
     /**
@@ -24,7 +31,12 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        $roles = Role::pluck('name', 'id');
+
+        return view('admin/user/create', [
+            'heading' => 'Create New User',
+            'roles'   => $roles,
+        ]);
     }
 
     /**
@@ -35,7 +47,29 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+        'name'      => 'bail|required|min:2',
+        'email'     => 'required|email|unique:users',
+        'bio'       => 'required',
+        'password'  => 'required|min:6',
+        'role'     => 'required'
+      ]);
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->bio = $request->bio;
+        $user->password = bcrypt($request->password);
+
+        if ($user->save()) {
+            // attach roles
+            $user->attachRole($request->role);
+            flash('User has been created');
+        } else {
+            flash()->error('Unable to create user');
+        }
+
+        return redirect()->route('users.index');
     }
 
     /**
@@ -57,7 +91,14 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::find($id);
+        $roles = Role::pluck('name', 'id');
+
+        return view('admin/user/edit', [
+            'heading' => 'Edit : '. $user->name,
+            'user'    => $user,
+            'roles'   => $roles,
+        ]);
     }
 
     /**
@@ -69,7 +110,28 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'name'  => 'bail|required|min:2',
+            'email' => 'required|email|unique:users,email,'. $id,
+            'role' => 'required'
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->fill($request->except('role', 'password'));
+
+        if ($request->password) {
+            $user->password = bcrypt($request->password);
+        }
+
+        if ($user->save()) {
+            $user->roles()->sync($request->role);
+            flash('User has been updated');
+        } else {
+            flash('Cannot update the user');
+        }
+
+        return redirect()->route('users.edit', $id);
     }
 
     /**
@@ -80,6 +142,27 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
+        if (Auth::user()->id ==$id) {
+            flash()->warning('Deletion of currently logged in user is not allowed :O')->important();
+            return redirect()->back();
+        }
+
+        $user = User::findOrFail($id);
+
+        // return nya selalu null
+        // karena method delete() telah di override oleh entrust
+        // yang jadi tidak ada return nya alias null
+        // Jancok entrust.
+        // if ($user->delete()) {
+        //     flash()->success('User has been deleted');
+        // } else {
+        //     flash()->error('Cannot delete user');
+        // }
         //
+
+        $user->delete();
+        flash()->success('User has been deleted');
+
+        return redirect()->route('users.index');
     }
 }
